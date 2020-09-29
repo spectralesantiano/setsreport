0c6fd874c5b46fa41f79875923c62d95b19a6994
spectralesantiano <erineil.santiano@spectralasia.net>
2020-09-29 13:33:55 +0800

    20200929 boostrap starting to apply. rptIndividualTestCertificate starting

--- MvcApplication3/Controllers/ReportPS/rptIndiTestResultController.cs
+++ MvcApplication3/Controllers/ReportPS/rptIndiTestResultController.cs
@@ -301,13 +301,15 @@ namespace SETSReport.Controllers.ReportPS
                                 searchText += String.Format("{0} LIKE '%{1}%'", namem, valuen);
                                 break;
                             case "DateTaken":
-                                DateTime frDate  = Convert.ToDateTime(valuen);
-                                searchText += String.Format("{0} >= '{1}'", namem, frDate);
+                                //DateTime frDate  = Convert.ToDateTime(valuen);
+                                //searchText += String.Format("{0} >= '{1}'", namem, frDate);
+                                searchText += String.Format("{0} >= '{1}'", namem, valuen);
                                 break;
                             case "ToDate":
                                 DateTime toDate  = Convert.ToDateTime(valuen);
                                 toDate = toDate.AddDays(1);
-                                searchText += String.Format("DateTaken <= '{0}'", toDate);
+                                //searchText += String.Format("DateTaken <= '{0}'", toDate);
+                                searchText += String.Format("DateTaken <= '{0}'", toDate.ToString("dd-MMM-yyyy"));
                                     break;
                             case "PositionID":
                                     searchText += String.Format("{0} = '{1}'", namem, valuen);
